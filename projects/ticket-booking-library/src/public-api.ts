691877008103d22bbe3f675ffe64aafcf577af6e
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-05-21 02:12:04 +0530

    Added final shared library checkin.

--- projects/ticket-booking-library/src/public-api.ts
+++ projects/ticket-booking-library/src/public-api.ts
@@ -5,3 +5,20 @@
 export * from './lib/ticket-booking-library.module';
 export * from './lib/models';
 export * from './lib/services';
+export * from './lib/shared/shared.module';
+export * from './lib/pipes/pipes.module';
+
+export * from './lib/shared/movie-detail/movie-detail.component';
+export * from './lib/shared/movie-details-card/movie-details-card.component';
+export * from './lib/shared/golden-text/golden-text.component';
+export * from './lib/shared/movie-preview-card/movie-preview-card.component';
+export * from './lib/shared/movie-search/movie-search.component';
+export * from './lib/shared/movies-grid/movies-grid.component';
+export * from './lib/shared/showtimes/showtimes.component';
+export * from './lib/shared/navbar/navbar.component';
+export * from './lib/shared/footer/footer.component';
+export * from './lib/pipes/day.pipe';
+export * from './lib/pipes/minute.pipe';
+export * from './lib/pipes/join.pipe';
+export * from './lib/pipes/safe.pipe';
+// export * from './lib/shared/movie-detail/movie-detail.component';
